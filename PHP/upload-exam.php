c1ff2bf550d0d1221a5d388e0d59ee5bc5afa01f
AdeleTheFarmer <james.haywood@lms.group>
2023-08-08 19:26:21 +0100

    Made more useful
    These enhancements will make the script more robust by handling potential errors, validating input, and adding some basic security checks.

--- PHP/upload-exam.php
+++ PHP/upload-exam.php
@@ -3,12 +3,25 @@
 $log_file_path = 'error.log';
 
 if ($_SERVER['CONTENT_TYPE'] == 'application/json') {
-    // Get the JSON data from the request
-    $jsonContent = file_get_contents('php://input');
-    $requestData = json_decode($jsonContent, true);
+    try {
+        // Get the JSON data from the request
+        $jsonContent = file_get_contents('php://input');
+        $requestData = json_decode($jsonContent, true);
 
-    // Check if JSON is valid and if fileName is set
-    if (json_last_error() == JSON_ERROR_NONE && isset($requestData['fileName']) && isset($requestData['data'])) {
+        // Validate JSON
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Invalid JSON data');
+        }
+
+        // Validate filename
+        if (!isset($requestData['fileName']) || !preg_match('/^[a-zA-Z0-9_-]+\.json$/', $requestData['fileName'])) {
+            throw new Exception('Invalid or missing file name');
+        }
+
+        // Validate data
+        if (!isset($requestData['data'])) {
+            throw new Exception('Missing data in request');
+        }
 
         // Get the file name and data
         $fileName = basename($requestData['fileName']); // Use basename to prevent path traversal attacks
@@ -17,20 +30,35 @@ if ($_SERVER['CONTENT_TYPE'] == 'application/json') {
         if (!is_dir('JSON')) {
             mkdir('JSON', 0777, true);
         }
-        
+
         $dir = dirname(dirname(__FILE__)); // Path to the folder containing the 'JSON' directory
         $filePath = $dir . '/JSON/' . $fileName; // Full path to the 'JSON' directory
+
+        // Check if the file already exists
+        if (file_exists($filePath)) {
+            // Create the archive directory name with the current date
+            $archive_directory = $dir . "/_Archive" . date('Ymd');
+
+            // Check if the archive directory exists, if not, create it
+            if (!file_exists($archive_directory)) {
+                mkdir($archive_directory, 0777, true);
+            }
+
+            // Move the existing file to the archive directory
+            $archive_file = $archive_directory . "/" . $fileName;
+            rename($filePath, $archive_file);
+        }
 
         // Save the file
         if (file_put_contents($filePath, $jsonData)) {
             echo 'File saved successfully';
         } else {
-            http_response_code(500);
-            echo 'An error occurred while saving the file';
+            throw new Exception('An error occurred while saving the file');
         }
-    } else {
-        http_response_code(400);
-        echo 'Invalid JSON data or missing file name';
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo $e->getMessage();
+        error_log($e->getMessage(), 3, $log_file_path);
     }
 } else {
     http_response_code(400);
