70ed09aab4ba02d8551d070159c8c3b62826261d
AdeleTheFarmer <james.haywood@lms.group>
2023-08-11 12:15:15 +0100

    Update generateExam.js

--- JS/generateExam.js
+++ JS/generateExam.js
@@ -109,7 +109,7 @@ $('#extractButton').on('click', function() {
         data: { imageUrl: imageUrl },
         success: function(response) {
             // Use Tesseract.js to process the locally saved image
-            imagePathLocal = "PHP/" + response;
+            imagePathLocal = "PHP/${response}";
             Tesseract.recognize(imagePathLocal)
                 .then(result => {
                     const extractedText = result.text;
