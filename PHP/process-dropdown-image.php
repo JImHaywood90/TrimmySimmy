d6070fddd7832b0ca534e6933031c66138fbf361
AdeleTheFarmer <james.haywood@lms.group>
2023-08-11 10:54:34 +0100

    Update process-dropdown-image.php

--- PHP/process-dropdown-image.php
+++ PHP/process-dropdown-image.php
@@ -5,4 +5,8 @@ $imageUrl = $_GET['imageUrl'];
 $tempImageFile = 'temp_image.jpg';
 file_put_contents($tempImageFile, file_get_contents($imageUrl));
 
+// Run Tesseract.js on the local image
+$ocrResult = shell_exec("tesseract $tempImageFile stdout");
+
+echo $ocrResult
 ?>
